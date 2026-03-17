92df2985bdd66d75c4e6dec1814e3d808652ec05
fadimariyam <fadeemariyam@gmail.com>
2026-03-17 13:18:20 +0400

    cors Fix - Check

--- server.js
+++ server.js
@@ -29,14 +29,33 @@ const app = express();
 // );
 
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
 
-app.options("*", cors());
+
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://transport-doc-front.vercel.app"
+  );
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+
 
 
 
