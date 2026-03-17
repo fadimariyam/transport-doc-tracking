ef6d68d12bb47fa42a941a6aa614f90713214636
fadimariyam <fadeemariyam@gmail.com>
2026-03-17 12:30:14 +0400

    Added Vercel URL

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app.vercel.app",
+      "https://transport-doc-front.vercel.app/",
     ],
     credentials: true,
   })
